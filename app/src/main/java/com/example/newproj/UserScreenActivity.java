9c2747c1b947a9494ce280f0c5534ee9bbf6180f
nadavco3 <Nadavcohen306@gmail.com>
2020-04-01 20:05:43 +0300

    Added an EditProfile screen My Profile screen design

--- app/src/main/java/com/example/newproj/UserScreenActivity.java
+++ app/src/main/java/com/example/newproj/UserScreenActivity.java
@@ -3,7 +3,10 @@ package com.example.newproj;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,8 +24,9 @@ import java.util.List;
 
 
 public class UserScreenActivity extends AppCompatActivity {
-    private TextView textName, textLastName, textAge, textEmail, textUserName;
+    private TextView textName, textDog, textType, textAge, textEmail, textUserName;
     private String name;
+    private ImageView editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +34,12 @@ public class UserScreenActivity extends AppCompatActivity {
         setContentView(R.layout.myprofile);
 
         textName = findViewById(R.id.profileName);
-        //textLastName = findViewById(R.id.profileLastName);
-        //textAge = findViewById(R.id.profileAge);
+        textDog = findViewById(R.id.dogName);
+        textType = findViewById(R.id.typeName);
+        textAge = findViewById(R.id.profileAge);
         textEmail = findViewById(R.id.profileEmail);
         textUserName = findViewById(R.id.userName);
+        editButton = findViewById(R.id.editProfile);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         DocumentReference user = db.collection("users").document(CurrentUser.currentUserEmail);
@@ -44,9 +50,10 @@ public class UserScreenActivity extends AppCompatActivity {
                     DocumentSnapshot doc = task.getResult();
                     textName.setText(doc.get("Name").toString() + " " + doc.get("LastName").toString());
                     textUserName.setText(textName.getText());
-                    //textLastName.setText(doc.get("LastName").toString());
-                    //textAge.setText(doc.get("Age").toString());
+                    textAge.setText(doc.get("Age").toString());
                     textEmail.setText(doc.get("Email").toString());
+                    textDog.setText(doc.get("DogName").toString());
+                    textType.setText(doc.get("DogType").toString());
                 }
             }
         })
@@ -56,10 +63,19 @@ public class UserScreenActivity extends AppCompatActivity {
                     }
                 });
 
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToEditProfile();
+            }
+        });
+
 
     }
 
-    private void initProfileData() {
+    private void goToEditProfile(){
+        Intent intent=new Intent(this,EditProfileActivity.class);
+        startActivity(intent);
     }
 
 
