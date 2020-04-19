5c20f6dbaa4bd88e7505c9a69b485d8fbee38d2b
nadavco3 <nadavcohen306@gmail.com>
2020-04-19 12:46:45 +0300

    PM2020T3-28 Actions (Type . to access issue actions)

---
+++ app/src/main/java/com/example/newproj/MeetingDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.newproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeetingDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting_details);
+    }
+}
